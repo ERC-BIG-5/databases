e271adcdd846320f5e9251bac9ce68bd6be63093
rsoleyma <ramin.soleymani@bsc.es>
2025-09-12 11:02:01 +0200

    hotfix

--- src/big5_databases/databases/c_db_merge.py
+++ src/big5_databases/databases/c_db_merge.py
@@ -6,12 +6,13 @@ from tqdm import tqdm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from big5_databases.databases.db_operations import filter_posts_with_existing_post_ids, get_tasks_with_posts
 from big5_databases.databases.meta_database import MetaDatabase
+from big5_databases.databases.model_conversion import CollectionTaskModel
 from .db_mgmt import DatabaseManager
 from .db_models import DBPost, DBCollectionTask
-from .db_utils import get_tasks_with_posts, filter_posts_with_existing_post_ids
 from .external import CollectionStatus
-
+from deepdiff import DeepHash
 
 @dataclass
 class MergeStats:
@@ -135,21 +136,102 @@ def check_for_conflicts(source: str | Path, target: str | Path) -> Dict[str, Any
     source_db = get_db(source)
     target_db = get_db(target)
 
+    print(f"{source_db.metadata.db_path=}")
+    print(f"{target_db.metadata.db_path=}")
+
     with source_db.get_session() as session:
-        source_tasks: set[DBCollectionTask] = set(session.execute(select(DBCollectionTask.task_name)).scalars())
+        source_tasks: set[str] = set(session.execute(select(DBCollectionTask.task_name)).scalars())
 
     with target_db.get_session() as session:
-        target_tasks: set[DBCollectionTask] = set(session.execute(select(DBCollectionTask.task_name)).scalars())
+        target_tasks: set[str] = set(session.execute(select(DBCollectionTask.task_name)).scalars())
 
     # tasks that are in the source but not in the target
-    print(source_tasks - target_tasks)
+    # print(source_tasks - target_tasks)
+
+    # compare_tasks = source_tasks - target_tasks
 
     # tasks that are in the source but not in the target
     existing = source_tasks & target_tasks
+
+    with source_db.get_session() as session:
+        f_source_tasks: list[CollectionTaskModel] = [t.model() for t in session.execute(select(DBCollectionTask).where(DBCollectionTask.task_name.in_(existing))).scalars().all()]
+
+    with target_db.get_session() as session:
+        f_target_tasks:  list[CollectionTaskModel] = [t.model() for t in session.execute(select(DBCollectionTask).where(DBCollectionTask.task_name.in_(existing))).scalars().all()]
+
+    src_map = {t.task_name: t for t in f_source_tasks}
+    tgt_map = {t.task_name: t for t in f_target_tasks}
+
+    diff_status = 0
+    diff_config = 0
+    diff_both_init = 0
+    diff_both_done = 0
+    diff_both_other_status = 0
+    # both done
+    diff_fount_posts = 0
+    # diff status
+    source_done_: list[CollectionTaskModel] = []
+    target_done_: list[CollectionTaskModel] = []
+    diff_both_other_status_: list[tuple[str,CollectionStatus,CollectionStatus]] = []
+
+    def update_task(from_db: DatabaseManager, to_db: DatabaseManager, from_task_model: CollectionTaskModel):
+        with from_db.get_session() as session:
+            new_posts = [p.model() for p in session.query(DBPost).filter(DBPost.collection_task_id == from_task_model.id).all()]
+
+        new_posts_pids = [p.platform_id for p in new_posts]
+        with to_db.get_session() as session:
+            target_obj = session.query(DBCollectionTask).filter(DBCollectionTask.task_name == from_task_model.task_name).scalar()
+            existing_posts = session.query(DBPost).filter(DBPost.platform_id.in_(new_posts_pids)).all()
+
+
     for t in existing:
+        src = src_map[t]
+        tgt = tgt_map[t]
+        source_conf = src.collection_config
+        target_conf = tgt.collection_config
+        s_hash = DeepHash(source_conf)[source_conf]
+        t_hash = DeepHash(target_conf)[target_conf]
+        if s_hash == t_hash:
+            if src.status == tgt.status:
+                if src.status == CollectionStatus.INIT:
+                    diff_both_init += 1
+                elif src.status == CollectionStatus.DONE:
+                    diff_both_done += 1
+                    if src.found_items != tgt.found_items:
+                        diff_fount_posts += 1
+                else:
+                    diff_both_other_status_.append((src.task_name, src.status,tgt.status))
 
-        t.collection_config
+            else:
+                diff_status += 1
+                if src.status == CollectionStatus.INIT and tgt.status == CollectionStatus.DONE :
+                    target_done_.append(tgt)
+                elif src.status == CollectionStatus.DONE and tgt.status == CollectionStatus.INIT:
+                    source_done_.append(src)
+                    update_task(source_db, target_db, src)
+                else:
+                    pass # other!
+        else:
+            print(f"Task {t} is different")
+            diff_config += 1
 
+    print("num match", len(existing))
+    print("---- same hash")
+    print("diff both init", diff_both_init)
+    print("diff both done", diff_both_done)
+    print("---")
+    print(f"{diff_fount_posts=}")
+    print("---")
+    print("diff both other status", diff_both_other_status)
+    print("diff status", diff_status)
+    print("---")
+    print(f"{len(source_done_)=}, {len(target_done_)=}")
+    print("src done",[t.task_name for t in source_done_])
+    print("target done",[t.task_name for t in target_done_])
+    print(f"{diff_both_other_status_=}")
+    print("---")
+    print("------ diff hash")
+    print("diff conf", diff_config)
 
 # Example usage:
 if __name__ == "__main__":
